546cd356fcf4fe050376cae3b0a98982271512a4
boris-ny <b.nyilindek@alustudent.com>
2023-02-23 12:56:01 +0200

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
-
-'''Write a Python script that, using this REST API, for a given employee ID, returns information about his/her Todo list progress.'''
+"""Module"""
 
 import requests
 import sys
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = 'https://jsonplaceholder.typicode.com/users/{}' .format(user_id)
-    user = requests.get(url).json()
-    todo_url = 'https://jsonplaceholder.typicode.com//users/{}/todos/'\
-        .format(user_id)
-    todo = requests.get(todo_url).json()
-    employee_name = user.get('name')
-    total_tasks = list(filter(lanbda x: x.get('completed') is True, todo))
-    tasks_completed = len(total_tasks)
-    total_number_of_tasks = len(todo)
 
-    print('Employee {} is done with tasks({}/{}):'\
-        .format(employee_name, tasks_completed, total_number_of_tasks))
-    
-    [print('\t' + task["title"]) for task in total_tasks]
+"""Module"""
+
+if __name__ == '__main__':
+    employee_id = sys.argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
+        .format(employee_id)
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
+        .format(employee_id)
+
+    user_info = requests.request('GET', user_url).json()
+    todos_info = requests.request('GET', todos_url).json()
+
+    employee_name = user_info["name"]
+    task_completed = list(filter(lambda obj:
+                                 (obj["completed"] is True), todos_info))
+    number_of_done_tasks = len(task_completed)
+    total_number_of_tasks = len(todos_info)
+
+    print("Employee {} is done with tasks({}/{}):".
+          format(employee_name, number_of_done_tasks, total_number_of_tasks))
+
+    [print("\t " + task["title"]) for task in task_completed]
