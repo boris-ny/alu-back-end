df42973bd71fa75588cd64fc13650c3d09e8e2a4
boris-ny <b.nyilindek@alustudent.com>
2023-02-23 12:36:23 +0200

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,11 +11,13 @@ if __name__ == "__main__":
     user = requests.get(url).json()
     todo_url = 'https://jsonplaceholder.typicode.com//users/{}/todos/'.format(user_id)
     todo = requests.get(todo_url).json()
+    
     done = []
+
     for task in todo:
         if task.get('completed') is True:
             done.append(task)
     print('Employee {} is done with tasks({}/{}):' .format(user.get('name'), len(done), len(todo)))
-    
+
     for task in done:
         print('\t {}' .format(task.get('title')))
